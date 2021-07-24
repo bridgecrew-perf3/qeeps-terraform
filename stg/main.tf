646f02ebf9b67ba788f3061766a2a6605c6b116b
Alin Florin <alin@qoffice.ro>
2021-07-24 16:40:50 +0300

    -

---
+++ stg/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-marsoffice"
+    storage_account_name = "samarsoffice"
+    container_name       = "tfstates"
+    key                  = "qeeps.stg.tfstate"
+  }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
