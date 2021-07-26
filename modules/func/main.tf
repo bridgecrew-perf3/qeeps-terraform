53369287952f6fb4691c005b57ab2acdf827c28c
Alin Florin <alin@qoffice.ro>
2021-07-27 02:04:26 +0300

    Update main.tf

--- modules/func/main.tf
+++ modules/func/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_function_app" "function_app" {
   auth_settings {
     enabled                       = true
     default_provider              = "AzureActiveDirectory"
+    issuer = "" TODO
     token_refresh_extension_hours = 24 * 30
     token_store_enabled           = true
     unauthenticated_client_action = "RedirectToLoginPage"
