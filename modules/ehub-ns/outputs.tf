afdd1754044699db9b35dd6a0b29190b6a0b35e9
Alin Florin <alin@qoffice.ro>
2021-08-05 01:54:26 +0300

    Removed ehub

--- modules/ehub-ns/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ns_authorization_rule_id" {
-  value = azurerm_eventhub_namespace_authorization_rule.nsrule.id
-}
-
-output "name" {
-  value = azurerm_eventhub_namespace.namespace.name
-}
--- modules/ehub/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "name" {
-  value = azurerm_eventhub.hub.name
-}
