ba4f6833962eac6bbce2d1ea8c844725041782ac
Alin Florin <alin@qoffice.ro>
2021-08-09 21:22:10 +0300

    d

--- modules/fd/main.tf
+++ modules/fd/main.tf
@@ -11,29 +11,17 @@ resource "azurerm_frontdoor" "front_door" {
   resource_group_name                          = var.resource_group
   enforce_backend_pools_certificate_name_check = false
 
-  # routing_rule {
-  #   name               = "redirectHttpHttps"
-  #   accepted_protocols = ["Http"]
-  #   patterns_to_match  = ["/*"]
-  #   frontend_endpoints = ["appFrontend"]
-  #   redirect_configuration {
-  #     redirect_protocol = "HttpsOnly"
-  #     redirect_type = "Found"
-  #   }
-  # }
-
   routing_rule {
-    name               = "swaRoute"
-    accepted_protocols = ["Https"]
+    name               = "redirectHttpHttps"
+    accepted_protocols = ["Http"]
     patterns_to_match  = ["/*"]
     frontend_endpoints = ["appFrontend"]
-    forwarding_configuration {
-      forwarding_protocol = "HttpsOnly"
-      backend_pool_name   = "swaBackendPool"
-      cache_enabled = true
-      cache_use_dynamic_compression = true
+    redirect_configuration {
+      redirect_protocol = "HttpsOnly"
+      redirect_type = "Found"
     }
   }
+
 
   routing_rule {
     name               = "swaRoute"
