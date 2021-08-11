4bafcc33367a04c6d93604a38e2842211bc14eaf
Alin Florin <alin@qoffice.ro>
2021-08-12 02:17:02 +0300

    f

--- modules/ad-app/outputs.tf
+++ modules/ad-app/outputs.tf
@@ -6,6 +6,10 @@ output "object_id" {
     value = azuread_application.application.object_id
 }
 
+output "sp_object_id" {
+    value = data.azuread_service_principal.enterprise_app.object_id
+}
+
 output "application_secret" {
     value = azuread_application_password.application_password.value
     sensitive = true
