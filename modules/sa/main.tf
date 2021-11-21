04eea82ba9bbe3da0dec48ae40c2fa120c023fff
Alin Florin <alin@qoffice.ro>
2021-11-21 12:52:10 +0200

    -

--- modules/sa/main.tf
+++ modules/sa/main.tf
@@ -13,11 +13,24 @@ resource "azurerm_storage_account" "storage_account" {
   location                 = var.location
   account_tier             = var.tier
   account_replication_type = var.replication_type
-  access_tier = var.access_tier
+  access_tier              = var.access_tier
+  blob_properties {
+    change_feed_enabled = true
+    versioning_enabled  = true
+  }
 }
 
 resource "azurerm_storage_container" "user_files_container" {
   name                  = "userfiles"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+
+  count = var.create_containers == true ? 1 : 0
+}
+
+
+resource "azurerm_storage_container" "graph_api_container" {
+  name                  = "graph-api"
   storage_account_name  = azurerm_storage_account.storage_account.name
   container_access_type = "private"
 
--- modules/zone/outputs.tf
+++ modules/zone/outputs.tf
@@ -1,23 +1,27 @@
 output "swa_name" {
-    value = module.swa.name
+  value = module.swa.name
 }
 
 output "swa_hostname" {
-    value = module.swa.hostname
+  value = module.swa.hostname
 }
 
 output "access_hostname" {
-    value = module.func_access.hostname
+  value = module.func_access.hostname
 }
 
 output "forms_hostname" {
-    value = module.func_forms.hostname
+  value = module.func_forms.hostname
 }
 
 output "notifications_hostname" {
-    value = module.func_notifications.hostname
+  value = module.func_notifications.hostname
 }
 
 output "files_hostname" {
-    value = module.func_files.hostname
+  value = module.func_files.hostname
+}
+
+output "sa_connection_string" {
+  value = module.sa.connection_string
 }
